ce6e24e26a74846542c065dae627bd8614b4311e
Stuart Winchester <stuart.winchester@gmail.com>
2016-06-15 21:01:50 +0100

    fix

--- src/main/java/org/swinchester/KubernetesProcessor.java
+++ src/main/java/org/swinchester/KubernetesProcessor.java
@@ -3,6 +3,8 @@ package org.swinchester;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 
 /**
@@ -13,10 +15,15 @@ public class KubernetesProcessor implements Processor {
     @Value("${KUBERNETES_NAMESPACE}")
     private String kubeNS;
 
+    Logger log = LoggerFactory.getLogger(KubernetesProcessor.class);
+
     DefaultKubernetesClient kubernetesClient = new DefaultKubernetesClient();
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        exchange.getIn().setBody(kubernetesClient.inNamespace(kubeNS).getNamespace());
+        log.info("calling kube api against " + kubeNS);
+        String something = kubernetesClient.inNamespace(kubeNS).lists().toString();
+        exchange.getIn().setBody(something);
+        log.info("result is : " + something);
     }
 }
