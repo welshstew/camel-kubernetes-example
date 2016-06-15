da34e1402571ac5318b6aa5dff6250c7c8ebc753
Stuart Winchester <stuart.winchester@gmail.com>
2016-06-15 20:23:22 +0100

    added KUBE NS value

--- src/main/java/org/swinchester/KubernetesProcessor.java
+++ src/main/java/org/swinchester/KubernetesProcessor.java
@@ -3,16 +3,20 @@ package org.swinchester;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * Created by swinchester on 15/06/16.
  */
 public class KubernetesProcessor implements Processor {
 
+    @Value("${KUBERNETES_NAMESPACE}")
+    private String kubeNS;
+
     DefaultKubernetesClient kubernetesClient = new DefaultKubernetesClient();
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        exchange.getIn().setBody(kubernetesClient.namespaces().list());
+        exchange.getIn().setBody(kubernetesClient.inNamespace(kubeNS).getNamespace());
     }
 }
